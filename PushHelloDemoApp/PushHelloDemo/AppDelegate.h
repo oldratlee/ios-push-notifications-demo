9b926843228866b97cac2633aabcb81c4c92d868
Jerry Lee <oldratlee@gmail.com>
2016-02-06 04:17:07 +0800

    Initial Commit

---
+++ PushHelloDemoApp/PushHelloDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushHelloDemo
+//
+//  Created by Jerry Lee on 16/2/6.
+//  Copyright © 2016年 Jerry Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushHelloDemoApp/PushHelloDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushHelloDemo
+//
+//  Created by Jerry Lee on 16/2/6.
+//  Copyright © 2016年 Jerry Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
